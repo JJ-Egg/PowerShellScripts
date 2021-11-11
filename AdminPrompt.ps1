8419d4723f23cbbdcda8b2991476fc0a8b5904a5
JJ-Egg <93942139+JJ-Egg@users.noreply.github.com>
2021-11-11 17:35:49 -0600

    Adding Source Information

--- AdminPrompt.ps1
+++ AdminPrompt.ps1
@@ -1,4 +1,6 @@
 ﻿#A snippet I incorporated into several scripts at work which needed administrator privileges to run. It prompts for an admin login before running anything else in the script. 
+ #Not created by me, yet I incorporated it into my own scripts
+ #Originally sourced from: https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html
 
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if([int]Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
